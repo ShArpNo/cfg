d5a0bfbf668f89c0abef76b0ab6f79395476a653
Sharp <sharpyeah@gmail.com>
2024-03-15 15:17:47 +0900

    add neovim

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,25 @@
+vim.opt.guicursor = ""
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set rnu")
+vim.cmd("set nu")
+
+-- undodir
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- enable incsearch
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.g.netrw_banner = 0 -- disable top help menu
+vim.g.netrw_liststyle = 3
