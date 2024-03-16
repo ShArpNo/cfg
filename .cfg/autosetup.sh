53f554721271beb9303c5f990d42982afb1aa6ea
user <user@asdasd.com>
2024-03-16 11:01:39 -0400

    change autosetup.sh

--- .cfg/autosetup.sh
+++ .cfg/autosetup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo ".cfg" >> .gitignore
 git clone --bare git@github.com:ShArpNo/cfg.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+
+function config {
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
 config checkout
-if [$? -ne 0] ; then
-  echo 'Backing up previous config...'
-  mkdir -p .config-backup && \
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-    xargs -I{} mv {} .config-backup/{}
-fi
+if [ $? -ne 0 ]; then
+  echo "Backing up previous config...";
+  mkdir -p .config-backup && config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+else
+  echo "No config found.";
+fi;
 config checkout
 config config --local status.showUntrackedFiles no
 
