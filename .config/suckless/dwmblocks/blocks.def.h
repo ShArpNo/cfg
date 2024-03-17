69a5522473175551e2f16c3565ab962799a4d00f
user <user@asdasd.com>
2024-03-17 00:27:16 -0400

    edit alacritty and dwm

--- .config/suckless/dwmblocks/blocks.def.h
+++ .config/suckless/dwmblocks/blocks.def.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"LANG: ", "xkb-switch",	2,		10},
-  {"VOL: ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1", 2, 10},
+	{"LANG: ", "xkb-switch",	2,		0},
+  {"VOL: ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1", 2, 0},
   {"", "date '+%a %d %b %Y'", 20, 0},
   {"", "date '+%T '", 1, 0},
 };
