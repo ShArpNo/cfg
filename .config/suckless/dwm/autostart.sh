239421e526d6cf8a2fe2eeaa186a474b1fc32f75
Sharp <sharpyeah@gmail.com>
2024-03-25 12:26:13 +0900

    fix java apps bug gray screen

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -3,4 +3,7 @@ setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle
 feh --bg-fill $HOME/wallpapers/cactus.png &
 xautolock -time 10 -locker slock &
 dwmblocks &
+export AWT_TOOLKIT=MToolKit
+export _JAVA_AWT_WM_NONREPARENTING=1
+wmname LG3D
 ibus exit
