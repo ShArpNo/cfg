26d7e60211d1dd3b2a16332ca5dca0b1b9a83a84
laptop <laptop@noemail.qwe>
2024-03-26 20:37:37 +0900

    alacritty gruvbox theme

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -1,6 +1,6 @@
 xrandr --output HDMI-0 --mode 1920x1080 &
 setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle
-feh --bg-fill $HOME/wallpapers/cactus.png &
+feh --bg-fill $HOME/wallpapers/a_foggy_forest_with_trees_03.jpg &
 xautolock -time 10 -locker slock &
 dwmblocks &
 export AWT_TOOLKIT=MToolKit
